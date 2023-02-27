d5e7a2b03b76c02c37d95f84e4a1beae2d89893f
Syed Umer Mujahide <syedsb1122@gmail.com>
2023-02-27 22:09:42 +0500

    hackathon

--- pages/Events.tsx
+++ pages/Events.tsx
@@ -1,8 +1,8 @@
 import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
-import { useEffect, useState } from "react"
-import { useRouter  } from "next/router";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
 import {
   Box,
   Flex,
@@ -31,7 +31,13 @@ import { Center } from "@chakra-ui/react";
 import { Card, CardHeader, CardBody, CardFooter } from "@chakra-ui/react";
 import { db } from "@/config/firebase";
 import { eventTypes } from "@/types/eventTypes";
-import { getDoc, collection, getDocs, deleteDoc, doc } from "firebase/firestore";
+import {
+  getDoc,
+  collection,
+  getDocs,
+  deleteDoc,
+  doc,
+} from "firebase/firestore";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Events() {
@@ -45,17 +51,19 @@ export default function Events() {
 
   const eventDeleteHandler = async (item: eventTypes) => {
     try {
-        // console.log(item.attachmentURL, item.attachmentURL.split('/'));
-        
-        // const desertRef = ref(storage, `todosImages/${item.description}.png`);
-        // await deleteObject(desertRef)
-        await deleteDoc(doc(db, "addEvent", item.id));
-        let filteredEvent = event.filter((event: eventTypes) => item.id !== event.id)
-        setEvent(filteredEvent)
+      // console.log(item.attachmentURL, item.attachmentURL.split('/'));
+
+      // const desertRef = ref(storage, `todosImages/${item.description}.png`);
+      // await deleteObject(desertRef)
+      await deleteDoc(doc(db, "addEvent", item.id));
+      let filteredEvent = event.filter(
+        (event: eventTypes) => item.id !== event.id
+      );
+      setEvent(filteredEvent);
     } catch (error) {
-        alert(error)
+      alert(error);
     }
-}
+  };
 
   const getEvents = async () => {
     try {
@@ -80,16 +88,15 @@ export default function Events() {
   const handleEventClick = (event: eventTypes) => {
     setSelectedEvent(event);
     onOpen();
-  }
+  };
 
   return (
-    
     <>
       <Head>
         <title>Events</title>
       </Head>
 
-      <Box bg="black" height={'100vh'}>
+      <Box bg="black" height={"100vh"}>
         <Center>
           <Box maxW="600px" p={4} m={6} textAlign="center">
             <Heading
@@ -103,46 +110,71 @@ export default function Events() {
             >
               Events
             </Heading>
-            <table style={{ padding: '10px'}}>
-  <thead>
-    <tr>
-      <th style={{  padding:'15px'}}><Heading>Title Events</Heading></th>
-      <th style={{  padding:'15px'}} colSpan={2}><Heading>Action</Heading></th>
-    </tr>  
-  </thead>
-  <tbody>
-    {event.map((event: eventTypes) => {
-      return (
-        <tr key={event.id}>
-          <td style={{  padding:'15px'}}>
-            <Button variant="link" onClick={() => handleEventClick(event)}><Heading size={'xl'} fontSize={'20'} padding={'10px'} borderRadius={'3px'} borderBottom={'2px var(--chakra-colors-chakra-body-bg) solid'} listStyleType={'none'} textDecoration={'none'} fontWeight={700}>{event.title}</Heading></Button>
-          </td>
-          <td style={{  padding:'15px'}}>
-            <Button onClick={() => eventDeleteHandler(event)}>Delete</Button>
-          </td>
-          <td style={{  padding:'15px'}}>
-          <Button>Update</Button>
-          </td>
-        </tr>
-      )
-    })}
-  </tbody>
-</table>
-
+            <table style={{ padding: "10px" }}>
+              <thead>
+                <tr>
+                  <th style={{ padding: "15px" }}>
+                    <Heading>Title Events</Heading>
+                  </th>
+                  <th style={{ padding: "15px" }} colSpan={2}>
+                    <Heading>Action</Heading>
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                {event.map((event: eventTypes) => {
+                  return (
+                    <tr key={event.id}>
+                      <td style={{ padding: "15px" }}>
+                        <Button
+                          variant="link"
+                          onClick={() => handleEventClick(event)}
+                        >
+                          <Heading
+                            size={"xl"}
+                            fontSize={"20"}
+                            padding={"10px"}
+                            borderRadius={"3px"}
+                            borderBottom={
+                              "2px var(--chakra-colors-chakra-body-bg) solid"
+                            }
+                            listStyleType={"none"}
+                            textDecoration={"none"}
+                            fontWeight={700}
+                          >
+                            {event.title}
+                          </Heading>
+                        </Button>
+                      </td>
+                      <td style={{ padding: "15px" }}>
+                        <Button onClick={() => eventDeleteHandler(event)}>
+                          Delete
+                        </Button>
+                      </td>
+                      <td style={{ padding: "15px" }}>
+                        <Button>Update</Button>
+                      </td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
           </Box>
-
         </Center>
       </Box>
 
       {selectedEvent && (
-        <Modal isOpen={isOpen} onClose={onClose} styles={{ overlay: { backgroundColor: 'rgba(255, 0, 0, 0.75)' }, content: { backgroundColor: 'red' } }}>
+        <Modal
+          isOpen={isOpen}
+          onClose={onClose}
+        >
           <ModalOverlay />
           <ModalContent>
             <ModalHeader>{selectedEvent.title}</ModalHeader>
             <ModalBody>
               <Text>
                 <b>Date:</b> {selectedEvent.date}
-                </Text>
+              </Text>
               <Text>
                 <b>Time:</b> {selectedEvent.time}
               </Text>
@@ -164,4 +196,3 @@ export default function Events() {
     </>
   );
 }
-
